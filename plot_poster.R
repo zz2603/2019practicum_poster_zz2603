ac96a170c370b5eacfc8d5af22bc7947cd1818a3
zz2603 <zz2603@cumc.columbia.edu>
2020-04-12 08:31:49 -0400

    Create a script to plot

---
+++ plot_poster.R
@@ -0,0 +1,10 @@
+featurePlot(x = x,
+            y = y,
+            scales = list(x=list(relation = "free"),
+                          y=list(relation = "free")),
+            plot = "density", pch = "|",
+            auto.key = list(columns = 2))
+
+pairs(dat[,1:10], pch = 19,  cex = 0.5,
+      col = my_cols[dat$false_positive],
+      lower.panel=NULL)
